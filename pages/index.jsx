d5505ca3d83f264fb62a44163f0a10436088596f
KoffeMeow <guyden999@mail.ru>
2022-07-19 22:27:19 +0300

    index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,30 @@
+import React, { Suspense } from 'react';
 import Head from 'next/head';
 import Header from 'components/header/header';
 import Footer from 'components/footer/footer';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+
+const startDate = new Date('2021-01-11');
+let today = new Date(new Date() - startDate);
+
+const Character = () => {
+	return(
+		<>
+			<OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+			<PerspectiveCamera makeDefault fov={20} position={[3, 2, 5]} />
+
+			<mesh>
+				<boxGeometry args={[1, 1, 1]} />
+				<meshBasicMaterial color={"white"} />
+			</mesh>
+		</>
+	);
+}
+
 
 const Home = () => {
+
 	return (
 		<>
 			<Head>
@@ -14,7 +36,19 @@ const Home = () => {
 			<Header />
 			
             <div className="body">
+				<div className="home__welcome">
+					<div className="home__welcome-info">
+						<h1>Lorem ipsum dolor sit amet</h1>
+						<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+						<p  className="date">Frontend developer: {((today.toISOString().slice(0, 4) - 1970) + "y " + (today.getMonth()) + "m " + today.getDate() + "d.")}</p>
+					</div>
 
+					<Suspense fallback={null}>
+						<Canvas className="canvas" shadows>
+							<Character/>
+						</Canvas>
+					</Suspense>
+				</div>
             </div>
 
 			<Footer />
@@ -23,3 +57,4 @@ const Home = () => {
 };
 
 export default Home;
+
