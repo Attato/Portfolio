3e638fd5a2a275e556f9c3b368a61f1bd9ac7075
KoffeMeow <guyden999@mail.ru>
2022-08-13 11:33:50 +0300

    tabs fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,53 +6,54 @@ import { motion } from 'framer-motion';
 
 const Home = () => {
 
-	const [page, setPage] = useState(0);
-
+	const [[page], setPage] = useState([0, 0]);
+	
 	useEffect(() => {
 		setPage(JSON.parse(localStorage.getItem('page number')));
+		console.log(JSON.parse(localStorage.getItem('page number')));
 	}, [])
 	
 	useEffect(() => {
 		localStorage.setItem('page number', JSON.stringify([page]));
 	}, [page])
-
+	
 	return (
-		<motion.div initial="hidden" whileInView="visible" translate="no" >
+		<motion.div initial="hidden" whileInView="visible" translate="no">
+
 			<Header />
-			<div className="sub">
-				{tabs.map(({ title }, wrap) => {
-					const isActive = wrap === page;
-					return (
-					<a
-					key={wrap}
-					onClick={() => {
-						setPage(wrap);
-					}}
-						className={isActive ? "active__tab" : ""}
-					>	
-						{title}
-					</a>
-					);
-				})}
-			</div>
-			<>
-				<div className="menu__sticky__wrap">
-					<div className="menu__button__wrap">									
-						
-					</div> 
-				</div>	
-				<motion.div
-					key={page}					
+
+			<div className="row">
+				<div className="sub">
+					<div className="sub__column">
+						{tabs.map(({ title }, i) => {
+							const isActive = i === page;
+							return (
+								<a
+									key={i}
+									onClick={() => {
+										setPage([i, i - page]);
+									}}
+									className={isActive ? 'hover' : ''}
+								>
+									{title}
+								</a>
+							);
+						})}
+					</div>
+				</div>
+
+				<motion.section
+					key={page}
 					initial={{ y: 10, opacity: 0 }}
 					animate={{ y: 0, opacity: 1 }}
-					transition={{ duration: 0.8, delay: 0 }}						
-				>								
+					transition={{ duration: 0.8, delay: 0 }}
+				>
 					{tabs[page].wrap}
-				</motion.div>				
-			</>
-			
+				</motion.section>
+			</div>
+
 			<Footer />
-        </motion.div>
+		</motion.div>
 	);
 };
 
